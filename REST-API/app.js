975da79fd47d41b9fd23bae86fafb8a92f36b687
gs6029907@gmail.com <gs6029907@gmail.com>
2025-12-10 11:17:32 +0530

    rest-api

--- REST-API/app.js
+++ REST-API/app.js
@@ -18,3 +18,12 @@ app.put('/users/:id' , (req, res)=>{
     const updateUser = req.body
     res.json({message : `User With ID ${userId} updated!`, updateUser})
 })
+
+app.delete('/users/:id' , (req , res)=>{
+    const userId = req.params.id
+    res.json({message : `User With ID ${userId} Deleted!`})
+})
+
+app.listen(PORT , ()=>{
+    console.log(`App Is Running on http://localhost:${PORT}`)
+})
