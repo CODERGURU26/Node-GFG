240f9d079d06f359b7337715802bb7c3a05e1333
gs6029907@gmail.com <gs6029907@gmail.com>
2025-12-10 11:06:32 +0530

    SESSIONINNODE

--- sessionInNode/app.js
+++ sessionInNode/app.js
@@ -1,44 +1,51 @@
-const express = require('express')
-const app = express()
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const PORT = 3000
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const PORT = 3000;
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(session({
-    secret : 'amar',
-    saveUninitialized : true,
-    resave : true
-}))
+    secret: 'amar',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// set view engine
+app.set('view engine', 'ejs');
 
 const user = {
-    name : 'Amar',
-    Roll_number : 45,
-    Address : 'Pune'
-}
+    name: 'Amar',
+    Roll_number: 45,
+    Address: 'Pune'
+};
 
-app.get('/' , (req , res)=>{
-    res.send("Welcome To The SessionDemo!")
-})
+// Home route
+app.get('/', (req, res) => {
+    res.render('home', { user: req.session.user });
+});
 
-app.get('/login' , (req , res)=>{
-    req.session.user = user
-    req.session.save()
-    return res.send("You Are Logged In")
-})
+// Login route
+app.get('/login', (req, res) => {
+    req.session.user = user;
+    req.session.save(() => {
+        res.redirect('/');
+    });
+});
 
-app.get('/user' , (req , res)=>{
-    const sessionUser = req.session.user
-    res.send(sessionUser)
-})
+// User route
+app.get('/user', (req, res) => {
+    res.render('user', { user: req.session.user });
+});
 
-app.get('/logout' , (req , res)=>{
-    req.session.destroy()
-    res.send("You Are Logged Out")
-    res.redirect('/')
-})
+// Logout route
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+});
 
-app.listen(PORT , ()=>{
-    console.log(`App Is Running On http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`App Is Running On http://localhost:${PORT}`);
+});
